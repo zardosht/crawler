7589487f84f310bcd4944f275500af79a1d50394
zardosht <zardosht@dorosht>
2011-06-08 23:07:07 +0200

    swt/jface libs
    Change-Id: I5f80b5148617b78a675a02e98686a80c2fb53d70

--- src/org/crawler/Main.java
+++ src/org/crawler/Main.java
@@ -1,11 +1,20 @@
 package org.crawler;
 
+
+import java.io.File;
+import java.util.List;
+
 import org.crawler.controler.GoogleCrawler;
+import org.crawler.model.ImportUtil;
 import org.crawler.model.Movie;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 public class Main {
 
@@ -16,7 +25,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		if (args.length > 0 && args[0].equals("--gui")) {
-			openGUI();
+			List<Movie> movies = ImportUtil.importMoviesFromFile(new File(
+					"data/u.item"));
+
+			openGUI(movies);
 		} else {
 
 			// List<Movie> movies = ImportUtil.importMoviesFromFile(new
@@ -37,13 +49,29 @@ public class Main {
 
 	}
 
-	private static void openGUI() {
+	private static void openGUI(List<Movie> movies) {
 		Display display = new Display ();
 		Shell shell = new Shell(display);
-	 
-		Text helloWorldTest = new Text(shell, SWT.NONE);
-		helloWorldTest.setText("Hello World SWT");
-		helloWorldTest.pack();
+		shell.setLayout(new GridLayout());
+		
+		
+		ListViewer lv = new ListViewer(shell, SWT.V_SCROLL | SWT.H_SCROLL);
+		lv.getList().setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true));
+		lv.setLabelProvider(new LabelProvider(){
+
+			@Override
+			public String getText(Object element) {
+				if(element instanceof Movie){
+					return ((Movie)element).getTitle();
+				}
+				return super.getText(element);
+			}
+			
+		});
+		
+		lv.setContentProvider(new ArrayContentProvider());
+		lv.setInput(movies);
+		
 	 
 		shell.open ();
 		while (!shell.isDisposed ()) {
