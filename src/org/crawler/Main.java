28101bdf660ce358696fb79c658178aede5d55fc
zardosht <zardosht@dorosht>
2011-06-08 14:04:26 +0200

    added swt body and lib
    Change-Id: I8e0821515215def49d0d1791cd2c297130e4f87a

--- src/org/crawler/Main.java
+++ src/org/crawler/Main.java
@@ -1,35 +1,55 @@
 package org.crawler;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
-
 import org.crawler.controler.GoogleCrawler;
-import org.crawler.model.ImportUtil;
 import org.crawler.model.Movie;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class Main {
 
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		
-//		List<Movie> movies = ImportUtil.importMoviesFromFile(new File("data/u.item"));
-	
-		GoogleCrawler crawler = new GoogleCrawler("http://imdb.com");
-		
-//		for(Movie movie : movies){
-//			crawler.getKeywords(movie);
-//		}
-		
-		for(String str : crawler.getKeywords(new Movie(null, "Goldeneye", null, null))) {
-			System.out.print(str+", ");
+
+		if (args.length > 0 && args[0].equals("--gui")) {
+			openGUI();
+		} else {
+
+			// List<Movie> movies = ImportUtil.importMoviesFromFile(new
+			// File("data/u.item"));
+
+			GoogleCrawler crawler = new GoogleCrawler("http://imdb.com");
+
+			// for(Movie movie : movies){
+			// crawler.getKeywords(movie);
+			// }
+
+			for (String str : crawler.getKeywords(new Movie(null, "Goldeneye",
+					null, null))) {
+				System.out.print(str + ", ");
+			}
+			System.out.println("");
 		}
-		System.out.println("");
-		
+
 	}
+
+	private static void openGUI() {
+		Display display = new Display ();
+		Shell shell = new Shell(display);
+	 
+		Text helloWorldTest = new Text(shell, SWT.NONE);
+		helloWorldTest.setText("Hello World SWT");
+		helloWorldTest.pack();
+	 
+		shell.open ();
+		while (!shell.isDisposed ()) {
+			if (!display.readAndDispatch ()) display.sleep ();
+		}
+		display.dispose ();
+	}		
 
 }
